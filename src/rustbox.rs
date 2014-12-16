ee10e2b0c59e96376d4ef8cafd5f076108c5a622
Joshua Yanovski <pythonesque@gmail.com>
2014-12-16 13:32:45 -0800

    Make RustBox actually thread safe.

--- src/rustbox.rs
+++ src/rustbox.rs
@@ -1,6 +1,9 @@
 extern crate libc;
 extern crate "termbox-sys" as termbox;
 
+pub use self::style::{Style, TB_BOLD, TB_UNDERLINE, TB_REVERSE};
+
+use std::sync::atomic::{mod, AtomicBool};
 use std::error::Error;
 use std::kinds::marker;
 
@@ -28,12 +31,6 @@ pub enum Color {
     White = TB_WHITE as int,
 }
 
-impl Color {
-    pub fn to_style(&self) -> Style {
-        Style { bits: *self as u16 & TB_NORMAL_COLOR.bits }
-    }
-}
-
 const TB_DEFAULT: u16 = 0x00;
 const TB_BLACK: u16 = 0x01;
 const TB_RED: u16 = 0x02;
@@ -44,20 +41,26 @@ const TB_MAGENTA: u16 = 0x06;
 const TB_CYAN: u16 = 0x07;
 const TB_WHITE: u16 = 0x08;
 
-bitflags! {
-    #[repr(C)]
-    flags Style: u16 {
-        const TB_NORMAL_COLOR = 0x000F,
-        const TB_BOLD = 0x0100,
-        const TB_UNDERLINE = 0x0200,
-        const TB_REVERSE = 0x0400,
-        const TB_ATTRIB = TB_BOLD.bits | TB_UNDERLINE.bits | TB_REVERSE.bits,
+mod style {
+    bitflags! {
+        #[repr(C)]
+        flags Style: u16 {
+            const TB_NORMAL_COLOR = 0x000F,
+            const TB_BOLD = 0x0100,
+            const TB_UNDERLINE = 0x0200,
+            const TB_REVERSE = 0x0400,
+            const TB_ATTRIB = TB_BOLD.bits | TB_UNDERLINE.bits | TB_REVERSE.bits,
+        }
+    }
+
+    impl Style {
+        pub fn from_color(color: super::Color) -> Style {
+            Style { bits: color as u16 & TB_NORMAL_COLOR.bits }
+        }
     }
 }
 
-fn nil_raw_event() -> RawEvent {
-    RawEvent{etype: 0, emod: 0, key: 0, ch: 0, w: 0, h: 0}
-}
+const NIL_RAW_EVENT: RawEvent = RawEvent { etype: 0, emod: 0, key: 0, ch: 0, w: 0, h: 0 };
 
 // FIXME: Rust doesn't support this enum representation.
 // #[deriving(Copy,FromPrimitive,Show)]
@@ -99,25 +102,39 @@ fn unpack_event(ev_type: c_int, ev: &RawEvent) -> EventResult<Event> {
 
 #[deriving(Copy,FromPrimitive,Show)]
 #[repr(C,int)]
-pub enum InitError {
-    Unknown = 0,
+pub enum InitErrorKind {
+    AlreadyOpen = 0,
     UnsupportedTerminal = -1,
     FailedToOpenTty = -2,
     PipeTrapError = -3,
 }
 
+pub type InitError = Option<InitErrorKind>;
+
 impl Error for InitError {
     fn description(&self) -> &str {
         match *self {
-            InitError::Unknown => "Unknown error.",
-            InitError::UnsupportedTerminal => "Unsupported terminal.",
-            InitError::FailedToOpenTty => "Failed to open TTY.",
-            InitError::PipeTrapError =>
+            Some(InitErrorKind::AlreadyOpen) => "Termbox is already open.",
+            Some(InitErrorKind::UnsupportedTerminal) => "Unsupported terminal.",
+            Some(InitErrorKind::FailedToOpenTty) => "Failed to open TTY.",
+            Some(InitErrorKind::PipeTrapError) =>
                 "Pipe trap error.  \
                  Termbox uses unix pipes in order to deliver a message from a signal handler to \
                  the main event reading loop.",
+            None => "Unknown error."
         }
     }
+}
+
+// The state of the RustBox is protected by the lock.  Yay, global state!
+static RUSTBOX_RUNNING: AtomicBool = atomic::INIT_ATOMIC_BOOL;
+
+/// true iff RustBox is currently running.  Beware of races here--don't rely on this for anything
+/// critical unless you happen to know that RustBox cannot change state when it is called (a good
+/// usecase would be checking to see if it's worth risking double printing backtraces to avoid
+/// having them swallowed up by RustBox).
+pub fn running() -> bool {
+    RUSTBOX_RUNNING.load(atomic::SeqCst)
 }
 
 #[allow(missing_copy_implementations)]
@@ -127,12 +144,23 @@ pub struct RustBox {
 
 impl RustBox {
     pub fn new() -> Result<RustBox, InitError> {
-        unsafe {
-            match termbox::tb_init() {
-                0 => Ok(RustBox { no_sync: marker::NoSync }),
-                res => match FromPrimitive::from_int(res as int) {
-                    Some(e) => Err(e),
-                    None => Err(InitError::Unknown)
+        // Ensure that we are not already running and simultaneously set RUSTBOX_RUNNING using an
+        // atomic swap.  This ensures that contending threads don't trample each other.
+        if RUSTBOX_RUNNING.swap(true, atomic::SeqCst) {
+            // The Rustbox was already running.
+            Err(Some(InitErrorKind::AlreadyOpen))
+        } else {
+            // The Rustbox was not already running.
+            unsafe {
+                match termbox::tb_init() {
+                    0 => Ok(RustBox { no_sync: marker::NoSync }),
+                    res => {
+                        // Remember to unset RUSTBOX_RUNNING.
+                        // Probably don't need SeqCst, but as noted elsewhere, better safe than
+                        // sorry.
+                        RUSTBOX_RUNNING.store(false, atomic::SeqCst);
+                        Err(FromPrimitive::from_int(res as int))
+                    }
                 }
             }
         }
@@ -164,8 +192,8 @@ impl RustBox {
     }
 
     pub fn print(&self, x: uint, y: uint, sty: Style, fg: Color, bg: Color, s: String) {
-        let fg = fg.to_style() | (sty & TB_ATTRIB);
-        let bg = bg.to_style();
+        let fg = Style::from_color(fg) | (sty & self::style::TB_ATTRIB);
+        let bg = Style::from_color(bg);
         for (i, ch) in s.as_slice().chars().enumerate() {
             unsafe {
                 self.change_cell(x+i, y, ch as u32, fg.bits(), bg.bits());
@@ -174,8 +202,8 @@ impl RustBox {
     }
 
     pub fn print_char(&self, x: uint, y: uint, sty: Style, fg: Color, bg: Color, ch: char) {
-        let fg = fg.to_style() | (sty & TB_ATTRIB);
-        let bg = bg.to_style();
+        let fg = Style::from_color(fg) | (sty & self::style::TB_ATTRIB);
+        let bg = Style::from_color(bg);
         unsafe {
             self.change_cell(x, y, ch as u32, fg.bits(), bg.bits());
         }
@@ -183,7 +211,7 @@ impl RustBox {
 
     pub fn poll_event(&self) -> EventResult<Event> {
         unsafe {
-            let ev = nil_raw_event();
+            let ev = NIL_RAW_EVENT;
             let rc = termbox::tb_poll_event(&ev as *const RawEvent);
             unpack_event(rc, &ev)
         }
@@ -192,7 +220,7 @@ impl RustBox {
     /// Returns Err(()) on EOF
     pub fn peek_event(&self, timeout: uint) -> EventResult<Event> {
         unsafe {
-            let ev = nil_raw_event();
+            let ev = NIL_RAW_EVENT;
             let rc = termbox::tb_peek_event(&ev as *const RawEvent, timeout as c_uint);
             unpack_event(rc, &ev)
         }
@@ -201,8 +229,14 @@ impl RustBox {
 
 impl Drop for RustBox {
     fn drop(&mut self) {
+        // Since only one instance of the RustBox is ever accessible, we should not
+        // need to do this atomically.
+        // Note: we should definitely have RUSTBOX_RUNNING = true here.
         unsafe {
             termbox::tb_shutdown();
         }
+        // Indicate that we're free now.  We could probably get away with lower atomicity here,
+        // but there's no reason to take that chance.
+        RUSTBOX_RUNNING.store(false, atomic::SeqCst);
     }
 }
